798af6267d289106c5c1755f044d7850b727130d
John Williams <jrwtango@gmail.com>
2020-08-21 05:16:17 -0400

    up4

---
+++ solution1.jsx
@@ -0,0 +1,119 @@
+const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+
+// sumulate getting products from DataBase
+const products = [
+  { name: "Apples_:", country: "Italy", cost: 3, instock: 10 },
+  { name: "Oranges:", country: "Spain", cost: 4, instock: 3 },
+  { name: "Beans__:", country: "USA", cost: 2, instock: 5 },
+  { name: "Cabbage:", country: "USA", cost: 1, instock: 8 },
+];
+const cartItems = [];
+
+//=========Cart=============
+const Cart = (props) => {
+  const { Card, Accordion, Button } = ReactBootstrap;
+  let data = props.location.data ? props.location.data : products;
+  console.log(`data:${JSON.stringify(data)}`);
+
+  const addTodo = (item) => {
+    const newTodos = [...cart, { item }];
+    setCart(newTodos);
+  };
+
+  return <Accordion defaultActiveKey="0">{list}</Accordion>;
+};
+
+const Products = (props) => {
+  const [items, setItems] = React.useState(products);
+  const [cart, setCart] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+  const {
+    Card,
+    Accordion,
+    Button,
+    Container,
+    Row,
+    Col,
+    Image,
+    Input,
+  } = ReactBootstrap;
+  const addToCart = (e) => {
+    let name = e.target.name;
+    let item = products.filter((item) => item.name == name);
+    console.log(`add to Cart ${JSON.stringify(item)}`);
+    setCart([...cart, ...item]);
+  };
+  const deleteItem = (e) => {
+    let name = e.target.innerHTML;
+    let x = cart.findIndex((item) => item.name == name);
+    console.log(`delete index ${x}`);
+    setCart(cart.splice(x, 1));
+  };
+  const photos = ["apple.png", "orange.png", "beans.png", "cabbage.png"];
+  let list = products.map((item, index) => {
+    //let n = index + 1049;
+    //let url = "https://picsum.photos/id/" + n + "/50/50";
+
+    return (
+      <li key={index}>
+        <Image src={photos[index]} width={70} roundedCircle></Image>
+        <Button variant="primary" size="large" onClick={addToCart}>
+          {item.name}:{item.cost}
+        </Button>
+        <input name={item.name} type="submit" onClick={addToCart}></input>
+      </li>
+    );
+  });
+  let cartList = cart.map((item, index) => {
+    return (
+      <Card key={index}>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey={1 + index}>
+            {item.name}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey={1 + index}>
+          <Card.Body>
+            $ {item.cost} from {item.country}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    );
+  });
+
+  let finalList = () => {
+    let final = cart.map((item) => {
+      return <div>{item.name}</div>;
+    });
+    return final;
+  };
+
+  const checkOut = () => {
+    let costs = cart.map((item) => item.cost);
+    const reducer = (accum, current) => accum + current;
+    setTotal(costs.reduce(reducer, 0));
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Product List</h1>
+          <ul style={{ listStyleType: "none" }}>{list}</ul>
+        </Col>
+        <Col>
+          <h1>Cart Contents</h1>
+          <Accordion>{cartList}</Accordion>
+        </Col>
+        <Col>
+          <h1>CheckOut </h1>
+          <Button onClick={checkOut}>CheckOut $ {total}</Button>
+          <div> {total && finalList()} </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+// ========================================
+ReactDOM.render(<Products />, document.getElementById("root"));
